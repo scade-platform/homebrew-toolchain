30aa920c8560bd7122d126abd99d4623d418eef0
Alexandr Esilevich <aeh@scade.io>
2024-08-30 04:27:39 +0100

    added scade-simulator cask

---
+++ Casks/scade-simulator.rb
@@ -0,0 +1,9 @@
+cask "scade-simulator" do
+  version "2.4.0"
+  sha256 "5c03a54d4d119c35ef62c1b39f859a1fc7e248c4189aa814f64ef33b67b1771a"
+
+  url "https://github.com/scade-platform/ScadeKit/releases/download/2.4.0/ScadeSimulator.app.tar.bz2"
+  name "scade-simulator"
+  desc "Scade Simulator"
+  homepage "https://www.scade.io"
+end
