42b7cc7d1448fafaafcb5b18dc36f8b8b5a295b3
Alexandr Esilevich <aeh@scade.io>
2024-08-30 04:02:39 +0100

    removed unversioned swift-android cask

--- Casks/swift-android.rb
+++
@@ -1,9 +0,0 @@
-cask "swift-android" do
-  version "5.10.1"
-  sha256 "4cda7eba2443f072c2ebf32b273f959362f22bffc1bc31d13439945c9fe24af2"
-
-  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/5.10.1/swift-android-compiler-5.10.1.tar.bz2"
-  name "swift-android"
-  desc "Swift Android Toolchain"
-  homepage "https://www.swift-android.com"
-end
