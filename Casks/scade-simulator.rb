499f5cf12535f7b7c70360cac1feb68682c953a4
Alexandr Esilevich <aeh@scade.io>
2024-09-16 01:56:23 +0100

    updated link and hash to scade simulator

--- Casks/scade-simulator.rb
+++ Casks/scade-simulator.rb
@@ -1,8 +1,8 @@
 cask "scade-simulator" do
   version "2.4.0"
-  sha256 "5c03a54d4d119c35ef62c1b39f859a1fc7e248c4189aa814f64ef33b67b1771a"
+  sha256 "7477294f1ede8017a766a1e334b7b732a451234b9520bdb7e8eacbf5681fa75b"
 
-  url "https://github.com/scade-platform/ScadeKit/releases/download/2.4.0/ScadeSimulator.app.tar.bz2"
+  url "https://github.com/scade-platform/ScadeKit/releases/download/2.4.0/ScadeSimulator.app.zip"
   name "scade-simulator"
   desc "Scade Simulator"
   homepage "https://www.scade.io"
