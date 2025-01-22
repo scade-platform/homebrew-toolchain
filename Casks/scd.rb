db8c53c3dfaf56dabba63a813d38365a5530a8d6
Alexandr Esilevich <aeh@scade.io>
2025-01-22 08:30:17 +0000

    updated scd package; added swift-6 packages

--- Casks/scd.rb
+++ Casks/scd.rb
@@ -1,11 +1,11 @@
 cask "scd" do
-  version "2.7.0"
-  sha256 "580e80cbad4c7cad6344b4023e419a72cb36cb4de32c1b513e7dac9b01a304ea"
+  version "2.9.0"
+  sha256 "12644f2ee97d81db1be90927df2dcc92391dfb6b31e4a7d537857a6fc1e6417b"
 
   depends_on formula: "cmake"
   depends_on formula: "gradle@7"
 
-  url "https://github.com/scade-platform/scade-build-tool/releases/download/2.7.0/scd-2.7.0.tar.bz2"
+  url "https://github.com/scade-platform/scade-build-tool/releases/download/2.9.0/scd-2.9.0.tar.bz2"
   name "scd"
   desc "Scade Build Tool"
   homepage "https://www.swift-android.com"
---
+++ Casks/swift-android@6.0.rb
@@ -0,0 +1,9 @@
+cask "swift-android@6.0" do
+  version "6.0.2"
+  sha256 "8b7d8744d9a308610f2a32a8104fb95a05e179a01e3fd12067367db520222fd4"
+
+  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.0.2/swift-android-compiler-6.0.2.tar.bz2"
+  name "swift-android@6.0"
+  desc "Swift Android Toolchain 6.0"
+  homepage "https://www.swift-android.com"
+end
