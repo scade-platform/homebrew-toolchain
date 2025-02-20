ea2132527e708ecd633130a79c90763b36851ee8
Alexandr Esilevich <aeh@scade.io>
2025-02-20 11:50:40 +0700

    added 6.0.2-1 package

--- Casks/swift-android@6.0.2-1.rb
+++ Casks/swift-android@6.0.2-1.rb
@@ -1,9 +1,9 @@
-cask "swift-android@6.0" do
+cask "swift-android@6.0.2-1" do
   version "6.0.2-1"
   sha256 "d8201294975d296fcff32f7feef8f3e33da2bda38535003b812ecee748d2454b"
 
   url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.0.2-1/swift-android-compiler-6.0.2-1.tar.bz2"
-  name "swift-android@6.0"
-  desc "Swift Android Toolchain 6.0"
+  name "swift-android@6.0.2-1"
+  desc "Swift Android Toolchain 6.0.2-1"
   homepage "https://www.swift-android.com"
 end
