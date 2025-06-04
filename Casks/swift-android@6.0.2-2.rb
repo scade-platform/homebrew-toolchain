a8e43cb5f83d5e867fd81d128e34f1773417704c
Alexandr Esilevich <aeh@scade.io>
2025-06-04 20:55:45 +0100

    added swift-android@6.0.2-2

---
+++ Casks/swift-android@6.0.2-2.rb
@@ -0,0 +1,9 @@
+cask "swift-android@6.0.2-2" do
+  version "6.0.2-2"
+  sha256 "80f77ae9965f169372c3dc58b3e8512baffe1e974de55605b5695cffdf64c7c4"
+
+  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.0.2-2/swift-android-compiler-6.0.2-2.tar.bz2"
+  name "swift-android@6.0.2-2"
+  desc "Swift Android Toolchain 6.0.2-2"
+  homepage "https://www.swift-android.com"
+end
