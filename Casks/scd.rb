98601a102aea57344244a7e2757bfb3e24177dc6
Alexandr Esilevich <aeh@scade.io>
2025-06-04 21:05:54 +0100

    updated scd version to 2.11.0

--- Casks/scd.rb
+++ Casks/scd.rb
@@ -1,11 +1,11 @@
 cask "scd" do
   version "2.10.0"
-  sha256 "20a6cc1145587d0e3842c6a97be6dea909cf9d81a9a136edaed5bca971c99d8f"
+  sha256 "c6cdf59c4feedf50127994b5164b5ca00f649392d622d61e289338b21d7dec4d"
 
   depends_on formula: "cmake"
   depends_on formula: "gradle"
 
-  url "https://github.com/scade-platform/scade-build-tool/releases/download/2.10.0/scd-2.10.0.tar.bz2"
+  url "https://github.com/scade-platform/scade-build-tool/releases/download/2.11.0/scd-2.11.0.tar.bz2"
   name "scd"
   desc "Scade Build Tool"
   homepage "https://www.swift-android.com"
