029763b41d3a01cedabb38e38e9f7e1a327447a3
Alexandr Esilevich <aeh@scade.io>
2025-07-14 20:07:57 +0100

    added 6.1.1 toolchain

--- Casks/swift-android@6.1.rb
+++ Casks/swift-android@6.1.rb
@@ -1,9 +1,9 @@
-cask "swift-android@6" do
+cask "swift-android@6.1" do
   version "6.1.1"
   sha256 "0b6a66ab3ff1941eb81bf39d9a2808b5ea70660ea701292ff3f8daa3161dc904"
 
   url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.1.1/swift-android-compiler-6.1.1.tar.bz2"
-  name "swift-android@6"
-  desc "Swift Android Toolchain 6"
+  name "swift-android@6.1"
+  desc "Swift Android Toolchain 6.1"
   homepage "https://www.swift-android.com"
 end
