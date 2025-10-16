70f87d46a491a19e630379e75418617eef073cab
Alexandr Esilevich <esil0x@gmail.comg>
2025-10-16 20:16:05 +0100

    added swift-6.1.1-2 toolchain

--- Casks/swift-android@6.1.1-2.rb
+++ Casks/swift-android@6.1.1-2.rb
@@ -1,9 +1,9 @@
-cask "swift-android@6.1.1" do
+cask "swift-android@6.1.1-2" do
   version "6.1.1-2"
   sha256 "d67ba42bb83ecb2db017aac210aff54b90c7e7a44100ed3675deeee57dc5df14"
 
   url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.1.1-2/swift-android-compiler-6.1.1-2.tar.bz2"
-  name "swift-android@6.1.1"
-  desc "Swift Android Toolchain 6.1.1"
+  name "swift-android@6.1.1-2"
+  desc "Swift Android Toolchain 6.1.1-2"
   homepage "https://www.swift-android.com"
 end
--- Casks/swift-android@6.rb
+++ Casks/swift-android@6.rb
@@ -1,8 +1,8 @@
 cask "swift-android@6" do
-  version "6.1.1-1"
-  sha256 "0b6a66ab3ff1941eb81bffae07f25454a4e893bc3b3750aaf38e73838e9f6275ee30d8ce77c4190ba23f439d9a2808b5ea70660ea701292ff3f8daa3161dc904"
+  version "6.1.1-2"
+  sha256 "d67ba42bb83ecb2db017aac210aff54b90c7e7a44100ed3675deeee57dc5df14"
 
-  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.1.1-1/swift-android-compiler-6.1.1-1.tar.bz2"
+  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.1.1-2/swift-android-compiler-6.1.1-2.tar.bz2"
   name "swift-android@6"
   desc "Swift Android Toolchain 6"
   homepage "https://www.swift-android.com"
