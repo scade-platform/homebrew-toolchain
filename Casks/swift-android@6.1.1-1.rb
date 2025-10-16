987d540ebf8e9343740ebe62943224452d5989fe
Alexandr Esilevich <esil0x@gmail.comg>
2025-10-16 20:15:53 +0100

    added swift-android@6.1.1-1

---
+++ Casks/swift-android@6.1.1-1.rb
@@ -0,0 +1,9 @@
+cask "swift-android@6.1.1-1" do
+  version "6.1.1-1"
+  sha256 "fae07f25454a4e893bc3b3750aaf38e73838e9f6275ee30d8ce77c4190ba23f4"
+
+  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.1.1-1/swift-android-compiler-6.1.1-1.tar.bz2"
+  name "swift-android@6.1.1-1"
+  desc "Swift Android Toolchain 6.1.1-1"
+  homepage "https://www.swift-android.com"
+end
