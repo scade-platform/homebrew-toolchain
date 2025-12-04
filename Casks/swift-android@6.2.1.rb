bf6df7acf937e3c90ecbee5e08a3131f7488be6e
Alexandr Esilevich <esil0x@gmail.comg>
2025-12-04 02:24:01 +0000

    added 6.2.1 toolchain

--- Casks/swift-android@6.2.1.rb
+++ Casks/swift-android@6.2.1.rb
@@ -1,9 +1,9 @@
-cask "swift-android@6" do
+cask "swift-android@6.2.1" do
   version "6.2.1"
   sha256 "ee8dafd8c978cd3ffd8cbb9ec36de0a8eb8c9d2daa79f6b000ec6787eaf0af0b"
 
   url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.2.1/swift-android-compiler-6.2.1.tar.bz2"
-  name "swift-android@6"
-  desc "Swift Android Toolchain 6"
+  name "swift-android@6.2.1"
+  desc "Swift Android Toolchain 6.2.1"
   homepage "https://www.swift-android.com"
 end
