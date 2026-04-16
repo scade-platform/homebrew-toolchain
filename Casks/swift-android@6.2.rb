b0f4f293c5f6b746bec530de226c50c9371a11dd
scdbot <97705525+scdbot@users.noreply.github.com>
2026-04-16 21:30:06 +0200

    Update swift-android@6.2.rb for 6.2.1-3

--- Casks/swift-android@6.2.rb
+++ Casks/swift-android@6.2.rb
@@ -1,8 +1,8 @@
 cask "swift-android@6.2" do
-  version "6.2.1-2"
-  sha256 "7bbb68d4145a643e8f4000b7a8b3f2877a783405ec245c761a955465f64dd0ac"
+  version "6.2.1-3"
+  sha256 "9d9813b523d0a3edbbf44da616606b27489c97d22308f7f6aaa65c8abaa563d8"
 
-  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.2.1-2/swift-android-compiler-6.2.1-2.tar.bz2"
+  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.2.1-3/swift-android-compiler-6.2.1-3.tar.bz2"
   name "swift-android@6.2"
   desc "Swift Android Toolchain 6.2"
   homepage "https://www.swift-android.com"
