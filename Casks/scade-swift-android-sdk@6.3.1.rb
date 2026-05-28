9b4aa54ff03457daac4fa6cbc40f356193ce8e7c
scdbot <97705525+scdbot@users.noreply.github.com>
2026-05-28 22:22:24 +0200

    Update scade-swift-android-sdk@6.3.1.rb for 6.3.1

---
+++ Casks/scade-swift-android-sdk@6.3.1.rb
@@ -0,0 +1,9 @@
+cask "scade-swift-android-sdk@6.3.1" do
+  version "6.3.1"
+  sha256 "8810527855b7ceeeedf77c3c23e9c51516990f7466cf0f715f140d37398eb55c"
+
+  url "https://github.com/scade-platform/swift-android-toolchain/releases/download/6.3.1/scade-swift-android-sdk-6.3.1.artifactbundle.zip"
+  name "scade-swift-android-sdk@6.3.1"
+  desc "SCADE Swift Android SDK 6.3.1"
+  homepage "https://www.swift-android.com"
+end
